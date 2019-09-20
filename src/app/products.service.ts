d6ec3e0af07c6d7d034e7e5f04b2d2c8b47cd5d2
Zoumbar <esteban.ouvre@devinci.fr>
2019-09-20 12:05:18 +0200

    edit and update fields

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,4 +1,3 @@
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -6,23 +5,41 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ProductsService {
-  getProducts() {
-    return this
-           .http
-           .get(`${this.uri}`);
-  }
+
   uri = 'http://localhost:4000/products';
 
   constructor(private http: HttpClient) { }
-  
+
   addProduct(ProductName, ProductDescription, ProductPrice) {
+    console.log(ProductName, ProductDescription, ProductPrice);
     const obj = {
       ProductName,
       ProductDescription,
       ProductPrice
     };
-    console.log(obj);
     this.http.post(`${this.uri}/add`, obj)
         .subscribe(res => console.log('Done'));
   }
+  updateProduct(ProductName, ProductDescription, ProductPrice, id) {
+    const obj = {
+      ProductName,
+      ProductDescription,
+      ProductPrice
+    };
+    this
+      .http
+      .post(`${this.uri}/update/${id}`, obj)
+      .subscribe(res => console.log('Done'));
+}
+  getProducts() {
+    return this
+           .http
+           .get(`${this.uri}`);
+  }
+
+  editProduct(id) {
+    return this
+            .http
+            .get(`${this.uri}/edit/${id}`);
+    }
 }
